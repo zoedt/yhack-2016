cbbe51241f80c86f8ad0c932674fe16e6d50e8a2
zoedt <ztiet@u.rochester.edu>
2016-11-12 16:10:37 -0500

    Cleaning up submitting info screen

--- Sprout/app/src/main/java/hu/ait/android/sprout/MainActivity.java
+++ Sprout/app/src/main/java/hu/ait/android/sprout/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPref.edit();
